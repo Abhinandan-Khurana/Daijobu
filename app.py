83b371d8ad7032e263f94fd7a60054c0cd052ffc
Abhinandan Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2023-01-09 02:05:08 +0530

    updated application

---
+++ app.py
@@ -0,0 +1,142 @@
+#Important Modules
+from flask import Flask,render_template, url_for ,flash , redirect
+import joblib
+from flask import request
+import numpy as np
+
+
+
+import os
+from flask import send_from_directory
+
+
+#from this import SQLAlchemy
+app=Flask(__name__,template_folder='template')
+
+
+
+# RELATED TO THE SQL DATABASE
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+#db=SQLAlchemy(app)
+
+#from model import User,Post
+
+#//////////////////////////////////////////////////////////
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+# UPLOAD_FOLDER = dir_path + '/uploads'
+# STATIC_FOLDER = dir_path + '/static'
+UPLOAD_FOLDER = 'uploads'
+STATIC_FOLDER = 'static'
+
+#graph = tf.get_default_graph()
+#with graph.as_default():;
+from tensorflow.keras.models import load_model
+model = load_model('model111.h5')
+model222=load_model("my_model.h5")
+
+#FOR THE FIRST MODEL
+
+# call model to predict an image
+def api(full_path):
+    data = image.load_img(full_path, target_size=(50, 50, 3))
+    data = np.expand_dims(data, axis=0)
+    data = data * 1.0 / 255
+
+    #with graph.as_default():
+    predicted = model.predict(data)
+    return predicted
+#FOR THE SECOND MODEL
+def api1(full_path):
+    data = image.load_img(full_path, target_size=(64, 64, 3))
+    data = np.expand_dims(data, axis=0)
+    data = data * 1.0 / 255
+
+    #with graph.as_default():
+    predicted = model222.predict(data)
+    return predicted
+
+
+# home page
+
+#@app.route('/')
+#def home():
+ #  return render_template('index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route("/")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+ 
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+@app.route("/diabetes")
+def diabetes():
+    #if form.validate_on_submit():
+    return render_template("diabetes.html")
+
+
+
+
+
+
+
+
+
+
+def ValuePredictor(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1,size)
+    if(size==8):#Diabetes
+        loaded_model = joblib.load("model1")
+        result = loaded_model.predict(to_predict)
+    return result[0]
+
+@app.route('/result',methods = ["POST"])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        if(len(to_predict_list)==30):#Cancer
+            result = ValuePredictor(to_predict_list,30)
+        elif(len(to_predict_list)==8):#Daiabtes
+            result = ValuePredictor(to_predict_list,8)
+        elif(len(to_predict_list)==12):
+            result = ValuePredictor(to_predict_list,12)
+        elif(len(to_predict_list)==11):
+            result = ValuePredictor(to_predict_list,11)
+            #if int(result)==1:
+            #   prediction ='diabetes'
+            #else:
+            #   prediction='Healthy' 
+        elif(len(to_predict_list)==10):
+            result = ValuePredictor(to_predict_list,10)
+    if(int(result)==1):
+        prediction='Sorry ! Suffering'
+    else:
+        prediction='Congrats ! you are Healthy' 
+    return(render_template("result.html", prediction=prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
